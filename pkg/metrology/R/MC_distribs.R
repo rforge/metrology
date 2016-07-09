e2ce832d08e66fabaa6383a1bee89fb25a7808a7
slre <slre@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-07-09 23:23:54 +0000

    Corrected dt.scaled
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/metrology@53 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/metrology/R/MC_distribs.R
+++ pkg/metrology/R/MC_distribs.R
@@ -62,7 +62,7 @@ rtri<-function(n, min=-sqrt(6), max=sqrt(6), mode=(min+max)/2) {
 #t
 dt.scaled<-function(x, df, mean=0, sd=1, ncp, log = FALSE) {
         if(!log) stats::dt((x-mean)/sd, df, ncp=ncp, log=FALSE)/sd
-        else stats::dt((x-mean)/sd, df, ncp=ncp, log=FALSE)- sd
+        else stats::dt((x-mean)/sd, df, ncp=ncp, log=TRUE)- log(sd)
 }
 pt.scaled<-function(q, df, mean=0, sd=1, ncp, lower.tail = TRUE, log.p = FALSE) {
         stats::pt((q-mean)/sd, df, ncp=ncp, log.p=log.p)
