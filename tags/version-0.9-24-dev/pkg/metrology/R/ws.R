6ddebe6d094f6b50adcbfebb47a3369ebc04c2c9
slre <slre@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2016-08-25 01:23:10 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/metrology@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tags/version-0.9-24-dev/pkg/metrology/R/ws.R
+++
@@ -1,8 +0,0 @@
-w.s<-function(ui, df, ci=rep(1, length(ui)), uc=sqrt(sum((ci*ui)^2)) ) {
-	(uc^4)/sum( ((ci*ui)^4) / df) 
-}
-
-welch.satterthwaite<-function( ui, df, ci=rep(1, length(ui)), uc=sqrt(sum((ci*ui)^2)) ) {
-	w.s(ui,df,ci,uc)
-}
-
